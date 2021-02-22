9b1a80b21fd38ccd0c11194d91ee8b9365cf20d6
Brent Acs <brent.mann@gmail.com>
2021-02-22 17:12:04 -0500

    Almost working, need to populate dropdown with initial value

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,7 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
-import {
-  PRIMARY_OUTLET,
-  Router,
-  UrlSegment,
-  UrlSegmentGroup,
-  UrlTree,
-} from '@angular/router';
-import { gitIgnoreTemplate } from './services/github-client-models';
+import { Component, OnInit, Input, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
 
+import { gitIgnoreTemplate } from './services/github-client-models';
 import { GithubClientService } from './services/github-client.service';
 
 @Component({
@@ -17,18 +10,9 @@ import { GithubClientService } from './services/github-client.service';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
-  gitIgnoreTemplate: gitIgnoreTemplate;
-  gitIngnoreTemplatesFormGroup = new FormGroup({
-    gitIgnoreTemplate: new FormControl(),
-    gitIgnoreTemplates: new FormControl(),
-  });
-
   title = 'gotch-ur-github';
+  currentTemplate: gitIgnoreTemplate;
   gitIgnoreTemplates: string[] = [];
-  // gitIgnoreTemplate: gitIgnoreTemplate = {
-  //   name: 'Sample',
-  //   source: 'some content explained\nand this explains it.',
-  // };
 
   constructor(
     private router: Router,
@@ -38,9 +22,10 @@ export class AppComponent implements OnInit {
   ngOnInit() {
     this.githubService.gitIgnoreTemplatesChanged.subscribe(
       (ignores: string[]) => {
-        // console.log('ignores:');
-        // console.log(ignores);
         this.gitIgnoreTemplates = ignores;
+        if (!this.currentTemplate) {
+          this.onChange(this.gitIgnoreTemplates[0]);
+        }
       }
     );
     this.githubService.getGitIgnoreTemplates();
@@ -49,25 +34,17 @@ export class AppComponent implements OnInit {
       (template: gitIgnoreTemplate) => {
         console.log('currentTemplateChanged()');
         console.log(template);
-        this.gitIgnoreTemplate = template;
+        this.currentTemplate = template;
       }
     );
   }
 
-  onChange(aValue) {
-    console.log('selected template changed: ');
-    console.log(this.gitIgnoreTemplate);
-    this.githubService.getGitIgnoreTemplate(this.gitIgnoreTemplate.name);
-  }
-
-  onChangeBACKUP(templateName) {
-    console.log('selected template changed: ' + templateName);
-    //this.githubService.getGitIgnoreTemplate(this.currentTemplate);
+  onChange(templateName) {
     this.githubService.getGitIgnoreTemplate(templateName);
   }
 
   onTest() {
-    this.gitIgnoreTemplate = {
+    this.currentTemplate = {
       name: 'Sample 2',
       source:
         'even more content and some content explained\nand this explains it.',
