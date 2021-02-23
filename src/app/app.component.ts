886caeaa9b1f5e1c23b759c87c29a0b3d577778f
Brent Acs <brent.mann@gmail.com>
2021-02-23 15:28:49 -0500

    Refactor the gitignore functionality to the actual component. needs styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,4 @@
-import { Component, OnInit, Input, ViewChild } from '@angular/core';
-import { Router } from '@angular/router';
-
-import { gitIgnoreTemplate } from './services/github-client-models';
-import { GithubClientService } from './services/github-client.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -10,47 +6,8 @@ import { GithubClientService } from './services/github-client.service';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
-  title = 'gotch-ur-github';
-  // selectedTemplate: gitIgnoreTemplate = {
-  //   name: '',
-  //   source: ''
-  // };
-  // gitIgnoreTemplates: string[] = [];
 
-  constructor(
-    private router: Router,
-    private githubService: GithubClientService
-  ) {}
+  constructor( ) {}
 
-  ngOnInit() {
-    // this.githubService.gitIgnoreTemplatesChanged.subscribe(
-    //   (ignores: string[]) => {
-    //     this.gitIgnoreTemplates = ignores;
-    //     if (this.selectedTemplate.name === '') {
-    //       this.onChange(this.gitIgnoreTemplates[0]);
-    //     }
-    //   }
-    // );
-    // this.githubService.getGitIgnoreTemplates();
-
-    // this.githubService.currentTemplateChanged.subscribe(
-    //   (template: gitIgnoreTemplate) => {
-    //     console.log('currentTemplateChanged()');
-    //     console.log(template);
-    //     this.selectedTemplate = template;
-    //   }
-    // );
-  }
-
-  onChange(templateName) {
-    // this.githubService.getGitIgnoreTemplate(templateName);
-  }
-
-  // onTest() {
-  //   this.selectedTemplate = {
-  //     name: 'Sample 2',
-  //     source:
-  //       'even more content and some content explained\nand this explains it.',
-  //   };
-  // }
+  ngOnInit() {}
 }
--- src/app/github/gitignore-templates/gitignore-templates.component.ts
+++ src/app/github/gitignore-templates/gitignore-templates.component.ts
@@ -1,15 +1,46 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { gitIgnoreTemplate } from 'src/app/services/github-client-models';
+import { GithubClientService } from 'src/app/services/github-client.service';
 
 @Component({
   selector: 'app-gitignore-templates',
   templateUrl: './gitignore-templates.component.html',
-  styleUrls: ['./gitignore-templates.component.css']
+  styleUrls: ['./gitignore-templates.component.css'],
 })
 export class GitignoreTemplatesComponent implements OnInit {
+  selectedTemplate: gitIgnoreTemplate = {
+    name: '',
+    source: '',
+  };
+  gitIgnoreTemplates: string[] = [];
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private githubService: GithubClientService
+  ) {}
 
   ngOnInit(): void {
+    this.githubService.gitIgnoreTemplatesChanged.subscribe(
+      (ignores: string[]) => {
+        this.gitIgnoreTemplates = ignores;
+        if (this.selectedTemplate.name === '') {
+          this.onChange(this.gitIgnoreTemplates[0]);
+        }
+      }
+    );
+    this.githubService.getGitIgnoreTemplates();
+    this.githubService.currentTemplateChanged.subscribe(
+      (template: gitIgnoreTemplate) => {
+        console.log('currentTemplateChanged()');
+        console.log(template);
+        this.selectedTemplate = template;
+      }
+    );
   }
 
+  onChange(templateName) {
+    this.githubService.getGitIgnoreTemplate(templateName);
+  }
 }
