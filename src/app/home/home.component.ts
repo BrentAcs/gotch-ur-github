33d480e4e371b7628b78ae9af5d7d6c165a8ef90
Brent Acs <brent.mann@gmail.com>
2021-02-23 23:21:48 -0500

    working on user info, but it's late, need to finish the clear user functionality.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
+import { tick } from '@angular/core/testing';
 import { NgForm } from '@angular/forms';
+import { CryptoService } from '../services/crypto/crypto.service';
+import { LocalStorageService } from '../services/local-storage/local-storage.service';
 
 @Component({
   selector: 'app-home',
@@ -9,27 +12,87 @@ import { NgForm } from '@angular/forms';
 export class HomeComponent implements OnInit {
   @ViewChild('f') homeForm: NgForm;
 
-  //appUser: AppUser = { name: 'Brent', accessToken: '' };
-  appUser: AppUser = { name: '', accessToken: '' };
-  appUsername = '';
+  appUser: AppUser = {
+    name: '',
+    accessToken: '',
+    secretKey: '',
+    persistSecretKey: false,
+  };
 
   constructor() {}
 
-  ngOnInit(): void {}
-
-  // onAppUsernameChange(){
-  //   const value = this.homeForm.value.
-  // }
+  // TODO: Determine if/how best practice for storing secret key
+  ngOnInit(): void {
+    this.loadAppUser();
+  }
 
   onSubmit() {
-    console.log('home.component#onSubmit()');
+    this.saveAppUser();
+  }
+
+  // TODO: Finish user persistance, need to work on clear functionality
+
+  onClearUser(){
+    this.homeForm.reset();
+    //this.saveAppUser();
+  }
+
+  private loadAppUser() {
+    this.appUser.name = LocalStorageService.getItem('appuser-name');
+    this.appUser.persistSecretKey = JSON.parse(
+      LocalStorageService.getItem('appuser-persist-secret-key')
+    );
+    const encryptedSecretKey = LocalStorageService.getItem(
+      'appuser-secret-key'
+    );
+    if (this.appUser.name && encryptedSecretKey) {
+      this.appUser.secretKey = CryptoService.decryptAES(
+        encryptedSecretKey,
+        this.appUser.name
+      );
+    }
+    const encryptedAccessToken = LocalStorageService.getItem(
+      'appuser-access-token'
+    );
+    if (this.appUser.secretKey && encryptedAccessToken) {
+      this.appUser.accessToken = CryptoService.decryptAES(
+        encryptedAccessToken,
+        this.appUser.secretKey
+      );
+    }
+  }
+
+  private saveAppUser() {
+    LocalStorageService.setItem('appuser-name', this.appUser.name);
+    LocalStorageService.setItem(
+      'appuser-persist-secret-key',
+      this.appUser.persistSecretKey.toString()
+    );
+    if (this.appUser.accessToken && this.appUser.secretKey) {
+      const encryptedAccessToken = CryptoService.encryptAES(
+        this.appUser.accessToken,
+        this.appUser.secretKey
+      );
+      LocalStorageService.setItem('appuser-access-token', encryptedAccessToken);
+    } else {
+      console.log('NOT persisting access token.');
+    }
+    if (this.appUser.secretKey && this.appUser.persistSecretKey) {
+      const encryptedSecretKey = CryptoService.encryptAES(
+        this.appUser.secretKey,
+        this.appUser.name
+      );
+      LocalStorageService.setItem('appuser-secret-key', encryptedSecretKey);
+    } else {
+      console.log('NOT persisting secret key.');
+    }
   }
 }
-
-
 
 // TODO: Determine best practice was of handling this.
 class AppUser {
   name: string;
   accessToken: string;
+  secretKey: string;
+  persistSecretKey: boolean;
 }
