2930774eab67ebead4949aab356e543e22655ab3
Brent Acs <brent.mann@gmail.com>
2021-02-26 12:08:14 -0500

    Removed need to content compoents to have app user/settings and services by removing stuff in the base. Everything appears to check out.

--- src/app/content/base-content/base-content.component.ts
+++ src/app/content/base-content/base-content.component.ts
@@ -1,9 +1,6 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Subscription } from 'rxjs';
 
 import { GithubClientService } from 'src/app/services/github/github-client.service';
-import { AppSettings } from 'src/app/shared/app-settings.model';
-import { AppUser } from 'src/app/shared/app-user.model';
 import { AppSettingsService } from 'src/app/services/app-settings/app-settings.service';
 import { AppUserService } from 'src/app/services/app-user/app-user.service';
 
@@ -13,47 +10,23 @@ import { AppUserService } from 'src/app/services/app-user/app-user.service';
   styleUrls: ['./base-content.component.css'],
 })
 export class BaseContentComponent implements OnInit, OnDestroy {
-  appUser: AppUser;
-  appUserSub: Subscription;
-  appSettings: AppSettings;
-  appSettingsSub: Subscription;
-
   constructor(
-    protected appUserService: AppUserService,
-    protected appSettingsService: AppSettingsService,
-    protected githubService: GithubClientService
+    public appUserService: AppUserService,
+    public appSettingsService: AppSettingsService,
+    public githubService: GithubClientService
   ) {}
 
   ngOnInit(): void {
     console.log('base OnInit');
 
-    this.appUserSub = this.appUserService.appUserChanged.subscribe(
-      (appUser) => {
-        console.log('base content, setting app user.')
-        this.appUser = appUser;
-      }
-    );
-
-    this.appSettingsSub = this.appSettingsService.appSettingsChanged.subscribe(
-      (appSettings) => {
-        console.log('base content, setting app settings.');
-        this.appSettings = appSettings;
-      }
-    );
-
     // TODO: is this the correct way of doing this??
     this.appUserService.appUserChanged.next(this.appUserService.appUser);
-    this.appSettingsService.appSettingsChanged.next(this.appSettingsService.appSettings);
+    this.appSettingsService.appSettingsChanged.next(
+      this.appSettingsService.appSettings
+    );
   }
 
   ngOnDestroy(): void {
-    console.log('base OnDestroy');
-
-    if (this.appUserSub) {
-      this.appUserSub.unsubscribe();
-    }
-    if (this.appSettingsSub) {
-      this.appSettingsSub.unsubscribe();
-    }
+    // console.log('base OnDestroy');
   }
 }
