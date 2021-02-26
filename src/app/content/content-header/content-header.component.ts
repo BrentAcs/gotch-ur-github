c5c51f7ba1395c3c58edf5c640680c3004069090
Brent Acs <brent.mann@gmail.com>
2021-02-26 11:59:46 -0500

    I *think* I fixed up the header by removing app user/settings objects and subscriptions and use the user/settings property of the respective services.

--- src/app/content/content-header/content-header.component.ts
+++ src/app/content/content-header/content-header.component.ts
@@ -1,37 +1,18 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Subscription } from 'rxjs';
+import { Component, OnInit } from '@angular/core';
 import { AppSettingsService } from 'src/app/services/app-settings/app-settings.service';
 import { AppUserService } from 'src/app/services/app-user/app-user.service';
-import { AppSettings } from 'src/app/shared/app-settings.model';
-import { AppUser } from 'src/app/shared/app-user.model';
 
 @Component({
   selector: 'app-content-header',
   templateUrl: './content-header.component.html',
   styleUrls: ['./content-header.component.css'],
 })
-export class ContentHeaderComponent implements OnInit, OnDestroy {
-  appUserSub: Subscription;
-  appSettingsSub: Subscription;
-  appUser: AppUser = new AppUser();
-  appSettings: AppSettings = new AppSettings();
-
+export class ContentHeaderComponent implements OnInit {
   constructor(
-    private appUserService: AppUserService,
-    private appSettingsService: AppSettingsService
+    public appUserService: AppUserService,
+    public appSettingsService: AppSettingsService
   ) {
-    this.appUserSub = this.appUserService.appUserChanged.subscribe(
-      (appUser) => {
-        this.appUser = appUser;
-      }
-    );
-
-    this.appSettingsSub = this.appSettingsService.appSettingsChanged.subscribe(
-      (appSettings) => {
-        this.appSettings = appSettings;
-      }
-    );
-  }
+    }
 
   // TODO: figure out how to hide appsettings for content that doesn't use it.
   // public get hasAppUserService() {
@@ -40,14 +21,8 @@ export class ContentHeaderComponent implements OnInit, OnDestroy {
 
   ngOnInit(): void {}
 
-  ngOnDestroy(): void {
-    this.appUserSub.unsubscribe();
-    this.appSettingsSub.unsubscribe();
-  }
-
-  // TODO: fix up and use property binding.
+  // TODO: not sure if this is the best way to handle, but hey, it's work to start with.
   onUserAccessTokenChange(event){
-    console.log('event.target.value: ' + event.target.value);
-    this.appSettingsService.appSettings.useAccessToken = event.target.value;
+    this.appSettingsService.save();
   }
 }
