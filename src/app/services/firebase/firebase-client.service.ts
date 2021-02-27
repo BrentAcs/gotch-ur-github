648a89915540eade1eff48b392d695a5f6a29aa6
BrentAcs <brent.mann@gmail.com>
2021-02-27 18:56:31 -0500

    working toward a firebase report.

--- src/app/services/firebase/firebase-client.service.ts
+++ src/app/services/firebase/firebase-client.service.ts
@@ -1,30 +1,61 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { AngularFireDatabase, AngularFireList } from '@angular/fire/database';
 
 import { AppUser } from 'src/app/shared/app-user.model';
-import { PersistedAppUser } from 'src/app/shared/persisted-app-user.model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class FirebaseClientService {
-  private baseUrl = 'https://gotchurgithub-default-rtdb.firebaseio.com/';
-  private appUsersCol = 'app-users.json';
+  appUserList: AngularFireList<AppUser> = null;
+  dbPath = '/app-users';
 
-  constructor(private http: HttpClient) {}
-
-  postAppUser(appUser: AppUser) {
-    this.http
-      .post(this.baseUrl + this.appUsersCol, appUser)
-      .subscribe((response) => {
-        console.log(response);
-      }),
-      (error) => {
-        console.error(error);
-      };
+  constructor(private db: AngularFireDatabase) {
+    this.appUserList = db.list(this.dbPath);
   }
 
-  getAppUser(name: string){
-    // this.http.get<PersistedAppUser>(this.baseUrl + this.appUsersCol);
+  getAll(): AngularFireList<AppUser> {
+    return this.appUserList;
   }
+
+  get(): ApoUser{
+    this.appUserList.que
+  }
+
+  create(appUser: AppUser): any {
+    return this.appUserList.push(appUser);
+  }
+
+  update(key: string, value: any): Promise<void> {
+    return this.appUserList.update(key, value);
+  }
+
+  delete(key: string): Promise<void> {
+    return this.appUserList.remove(key);
+  }
+
+  deleteAll(): Promise<void> {
+    return this.appUserList.remove();
+  }
+
+  // private baseUrl = 'https://gotchurgithub-default-rtdb.firebaseio.com/';
+  // private appUsersCol = 'app-users.json';
+
+  // constructor(private http: HttpClient) {}
+
+  // postAppUser(appUser: AppUser) {
+  //   this.http
+  //     .post(this.baseUrl + this.appUsersCol, appUser)
+  //     .subscribe((response) => {
+  //       console.log(response);
+  //     }),
+  //     (error) => {
+  //       console.error(error);
+  //     };
+  // }
+
+  // getAppUser(name: string){
+  //   // this.http.get<PersistedAppUser>(this.baseUrl + this.appUsersCol);
+  // }
 }
