68a36864176dcfd2614c6be756eb10f47f45f0ed
Brent Acs <brent.mann@gmail.com>
2021-02-27 11:25:50 -0500

    playing around w/ firebase at the bare metal level. want a panic commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 import { AppSettingsService } from './services/app-settings/app-settings.service';
 import { AppUserService } from './services/app-user/app-user.service';
+import { FirebaseClientService } from './services/firebase/firebase-client.service';
 import { ConsoleLoggingService, ILoggingService } from './services/logging-service/logging.service';
 import { AppUser } from './shared/app-user.model';
 import { PersistedAppUser } from './shared/persisted-app-user.model';
@@ -14,7 +15,8 @@ import { PersistedAppUser } from './shared/persisted-app-user.model';
 export class AppComponent implements OnInit {
   constructor(
     private appUserService: AppUserService,
-    private appSettingsService: AppSettingsService
+    private appSettingsService: AppSettingsService,
+    private firebase: FirebaseClientService,
   ) {}
 
   ngOnInit() {
@@ -23,19 +25,32 @@ export class AppComponent implements OnInit {
 
     const user = new AppUser(
       'Brent',
-      '962989e3968bbef8398f0238d6dc3784d827b799',
+      'Test-acces-token-please-ignore',
       true,
       'kitty',
       true
     );
-    console.log('app user:');
-    console.log(user);
+    const user2 = new AppUser(
+      'Connor',
+      'Test-acces-token-please-ignore-2',
+      true,
+      'lucas',
+      true
+    );
+    // console.log('app user:');
+    // console.log(user);
 
     const persistedUser = PersistedAppUser.toPersisted(user);
-    console.log('persisted app user:');
-    console.log(persistedUser);
+    const persistedUser2 = PersistedAppUser.toPersisted(user2);
+    // console.log('persisted app user:');
+    // console.log(persistedUser);
 
-    const user2 = PersistedAppUser.fromPersisted(persistedUser);
+    console.log('test posting app user.');
+    this.firebase.postAppUser(persistedUser);
+    this.firebase.postAppUser(persistedUser2);
+
+    const user10 = PersistedAppUser.fromPersisted(persistedUser);
+    const user11 = PersistedAppUser.fromPersisted(persistedUser2);
     console.log('app user 2:');
     console.log(user2);
   }
--- src/app/services/firebase/firebase-client.service.ts
+++ src/app/services/firebase/firebase-client.service.ts
@@ -1,10 +1,30 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+
+import { AppUser } from 'src/app/shared/app-user.model';
+import { PersistedAppUser } from 'src/app/shared/persisted-app-user.model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class FirebaseClientService {
   private baseUrl = 'https://gotchurgithub-default-rtdb.firebaseio.com/';
+  private appUsersCol = 'app-users.json';
 
-  constructor() {}
+  constructor(private http: HttpClient) {}
+
+  postAppUser(appUser: AppUser) {
+    this.http
+      .post(this.baseUrl + this.appUsersCol, appUser)
+      .subscribe((response) => {
+        console.log(response);
+      }),
+      (error) => {
+        console.error(error);
+      };
+  }
+
+  getAppUser(name: string){
+    // this.http.get<PersistedAppUser>(this.baseUrl + this.appUsersCol);
+  }
 }
