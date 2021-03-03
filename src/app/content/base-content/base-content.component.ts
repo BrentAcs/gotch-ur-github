cd4e353a00159c3660440090a6302d12d5b48032
Brent Acs <brent.mann@gmail.com>
2021-03-02 20:09:22 -0500

    Huge headway in the re-orig of how I'm managing app users. still work todo.

--- src/app/content/base-content/base-content.component.ts
+++ src/app/content/base-content/base-content.component.ts
@@ -20,50 +20,6 @@ export class BaseContentComponent implements OnInit, OnDestroy {
     return this.appUsersService.selectedAppUser !== null;
   }
 
-  get selectedUserAppName() {
-    return this.hasSelectedAppUser
-      ? this.appUsersService.selectedAppUser.name
-      : '';
-  }
-  set selectedUserAppName(value: string) {
-    this.hasSelectedAppUser
-      ? (this.appUsersService.selectedAppUser.name = value)
-      : null;
-  }
-
-  get selectedUserAppAccessToken() {
-    return this.hasSelectedAppUser
-      ? this.appUsersService.selectedAppUser.accessToken
-      : '';
-  }
-  set selectedUserAppAccessToken(value: string) {
-    this.hasSelectedAppUser
-      ? (this.appUsersService.selectedAppUser.accessToken = value)
-      : null;
-  }
-
-  get selectedUserAppSecretKey() {
-    return this.hasSelectedAppUser
-      ? this.appUsersService.selectedAppUser.secretKey
-      : '';
-  }
-  set selectedUserAppSecretKey(value: string) {
-    this.hasSelectedAppUser
-      ? (this.appUsersService.selectedAppUser.secretKey = value)
-      : null;
-  }
-
-  get selectedUserAppPersistSecretKey() {
-    return this.hasSelectedAppUser
-      ? this.appUsersService.selectedAppUser.persistSecretKey
-      : false;
-  }
-  set selectedUserAppPersistSecretKey(value: boolean) {
-    this.hasSelectedAppUser
-      ? (this.appUsersService.selectedAppUser.persistSecretKey = value)
-      : null;
-  }
-
   ngOnInit(): void {
     // TODO: is this the correct way of doing this??
     this.appUsersService.selectedAppUserChanged.next(
